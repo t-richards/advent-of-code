bf4e377227d8f8748182ddefee6167fc3edd820e
Tom Richards <tom@tomrichards.net>
2021-11-30 23:52:43 -0500

    Add 2021 scaffold

---
+++ 2021/day01/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var input []int
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		num, _ := strconv.Atoi(scanner.Text())
+		input = append(input, num)
+	}
+
+	fmt.Println(SolvePart1(input))
+	fmt.Println(SolvePart2(input))
+}
+
+func SolvePart1(input []int) int {
+	return -1
+}
+
+func SolvePart2(input []int) int {
+	return -1
+}
---
+++ 2021/day01/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPart1(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	actual := SolvePart1(input)
+
+	assert.Equal(t, -1, actual)
+}
+
+func TestPart2(t *testing.T) {
+	input := []int{4, 5, 6}
+
+	actual := SolvePart1(input)
+
+	assert.Equal(t, -1, actual)
+}
