d37cce3e9a660bb7dbd183570e84aceac4012f42
Tom Richards <tom@tomrichards.net>
2021-12-02 00:08:04 -0500

    Day 2

--- 2021/day02/main.go
+++ 2021/day02/main.go
@@ -5,25 +5,61 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	var input []int
+	var input []string
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		num, _ := strconv.Atoi(scanner.Text())
-		input = append(input, num)
+		input = append(input, scanner.Text())
 	}
 
 	fmt.Println(SolvePart1(input))
 	fmt.Println(SolvePart2(input))
 }
 
-func SolvePart1(input []int) int {
-	return 0
+func SolvePart1(input []string) int {
+	hor := 0
+	dep := 0
+
+	for _, line := range input {
+		parts := strings.Split(line, " ")
+		amount, _ := strconv.Atoi(parts[1])
+
+		switch parts[0] {
+		case "forward":
+			hor += amount
+		case "down":
+			dep += amount
+		case "up":
+			dep -= amount
+		}
+	}
+
+	return hor * dep
 }
 
-func SolvePart2(input []int) int {
-	return 0
+func SolvePart2(input []string) int {
+	hor := 0
+	dep := 0
+	aim := 0
+
+	for _, line := range input {
+		parts := strings.Split(line, " ")
+		amount, _ := strconv.Atoi(parts[1])
+
+		switch parts[0] {
+		case "down":
+			aim += amount
+		case "up":
+			aim -= amount
+		case "forward":
+			hor += amount
+			dep += aim * amount
+		}
+	}
+
+	return hor * dep
 }
--- 2021/day02/main_test.go
+++ 2021/day02/main_test.go
@@ -6,16 +6,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var exampleInput = []int{1, 2, 3}
+var exampleInput = []string{
+	"forward 5",
+	"down 5",
+	"forward 8",
+	"up 3",
+	"down 8",
+	"forward 2",
+}
 
 func TestPart1(t *testing.T) {
 	actual := SolvePart1(exampleInput)
 
-	assert.Equal(t, 7, actual)
+	assert.Equal(t, 150, actual)
 }
 
 func TestPart2(t *testing.T) {
 	actual := SolvePart2(exampleInput)
 
-	assert.Equal(t, 5, actual)
+	assert.Equal(t, 900, actual)
 }
