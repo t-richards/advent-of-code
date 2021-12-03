2a3f088382449f72b3c7ccb78a7de44ee887e40c
Tom Richards <tom@tomrichards.net>
2021-12-03 01:19:50 -0500

    Day 3 part 1

--- 2021/day03/day03.go
+++ 2021/day03/day03.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -18,14 +20,38 @@ func main() {
 	fmt.Println(SolvePart2(input))
 }
 
+// Janky string version, not sorry
 func SolvePart1(input []string) int {
-	aaaaaaa := 0
+	var gamma strings.Builder
+	var epsilon strings.Builder
 
-	return aaaaaaa
+	for col := 0; col < len(input[0]); col++ {
+		zeroCnt := 0
+		oneCnt := 0
+
+		for row := 0; row < len(input); row++ {
+			if input[row][col] == '0' {
+				zeroCnt++
+			} else {
+				oneCnt++
+			}
+		}
+
+		if zeroCnt > oneCnt {
+			gamma.WriteRune('1')
+			epsilon.WriteRune('0')
+		} else {
+			gamma.WriteRune('0')
+			epsilon.WriteRune('1')
+		}
+	}
+
+	gam, _ := strconv.ParseInt(gamma.String(), 2, 64)
+	eps, _ := strconv.ParseInt(epsilon.String(), 2, 64)
+
+	return int(gam * eps)
 }
 
-func SolvePart2(input []string) int {
-	bbbbbbbb := 0
-
-	return bbbbbbbb
+func SolvePart2(input []string) int64 {
+	return 0
 }
--- 2021/day03/day03_test.go
+++ 2021/day03/day03_test.go
@@ -6,16 +6,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var exampleInput = []string{}
+var exampleInput = []string{
+	"00100",
+	"11110",
+	"10110",
+	"10111",
+	"10101",
+	"01111",
+	"00111",
+	"11100",
+	"10000",
+	"11001",
+	"00010",
+	"01010",
+}
 
 func TestPart1(t *testing.T) {
 	actual := SolvePart1(exampleInput)
 
-	assert.Equal(t, 1111111, actual)
+	assert.Equal(t, 198, actual)
 }
 
 func TestPart2(t *testing.T) {
 	actual := SolvePart2(exampleInput)
 
-	assert.Equal(t, 22222222, actual)
+	assert.Equal(t, 230, actual)
 }
