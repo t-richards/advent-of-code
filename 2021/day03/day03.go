06192f57d4dbdeee0c3e18e1f501ada8755c4d6b
Tom Richards <tom@tomrichards.net>
2021-12-03 19:40:33 -0500

    Add rowlen

--- 2021/day03/day03.go
+++ 2021/day03/day03.go
@@ -21,11 +21,12 @@ func main() {
 }
 
 func transpose(input []string) []string {
+	rowLen := len(input)
 	columnLen := len(input[0])
 	result := make([]string, columnLen)
 
 	for col := 0; col < columnLen; col++ {
-		for row := 0; row < len(input); row++ {
+		for row := 0; row < rowLen; row++ {
 			result[col] += string(input[row][col])
 		}
 	}
