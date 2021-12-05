29af50655b9aa936df839afebe945c16ac3213aa
Tom Richards <tom@tomrichards.net>
2021-12-05 14:59:24 -0500

    Day 5 part 1

--- 2021/day05/day05.go
+++ 2021/day05/day05.go
@@ -3,6 +3,9 @@ package main
 import (
 	"aoc/internal/data"
 	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 func main() {
@@ -12,8 +15,94 @@ func main() {
 	fmt.Println(SolvePart2(input))
 }
 
+type point struct {
+	x int
+	y int
+}
+
+type line struct {
+	p1 point
+	p2 point
+}
+
+func (l line) points() []point {
+	result := []point{}
+
+	xmin, xmax := minmax(l.p1.x, l.p2.x)
+	ymin, ymax := minmax(l.p1.y, l.p2.y)
+
+	for x := xmin; x <= xmax; x++ {
+		for y := ymin; y <= ymax; y++ {
+			pt := point{x: x, y: y}
+			result = append(result, pt)
+		}
+	}
+
+	return result
+}
+
+func (l line) vertical() bool {
+	return l.p1.x == l.p2.x
+}
+
+func (l line) horizontal() bool {
+	return l.p1.y == l.p2.y
+}
+
+func minmax(x, y int) (int, int) {
+	if x < y {
+		return x, y
+	}
+
+	return y, x
+}
+
+func toi(s string) int {
+	n, _ := strconv.Atoi(s)
+	return n
+}
+
+func numeric(c rune) bool {
+	return !unicode.IsNumber(c)
+}
+
 func SolvePart1(input []string) int {
-	return 0
+	lines := []line{}
+	for _, s := range input {
+		parts := strings.FieldsFunc(s, numeric)
+
+		l := line{
+			p1: point{
+				x: toi(parts[0]),
+				y: toi(parts[1]),
+			},
+			p2: point{
+				x: toi(parts[2]),
+				y: toi(parts[3]),
+			},
+		}
+
+		// only consider horizontal and vertical lines
+		if l.vertical() || l.horizontal() {
+			lines = append(lines, l)
+		}
+	}
+
+	vents := map[point]int{}
+	for _, line := range lines {
+		for _, point := range line.points() {
+			vents[point] += 1
+		}
+	}
+
+	count := 0
+	for _, v := range vents {
+		if v > 1 {
+			count++
+		}
+	}
+
+	return count
 }
 
 func SolvePart2(input []string) int {
--- 2021/day05/day05_test.go
+++ 2021/day05/day05_test.go
@@ -6,12 +6,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var exampleInput = []string{}
+var exampleInput = []string{
+	"0,9 -> 5,9",
+	"8,0 -> 0,8",
+	"9,4 -> 3,4",
+	"2,2 -> 2,1",
+	"7,0 -> 7,4",
+	"6,4 -> 2,0",
+	"0,9 -> 2,9",
+	"3,4 -> 1,4",
+	"0,0 -> 8,8",
+	"5,5 -> 8,2",
+}
 
 func TestPart1(t *testing.T) {
 	result := SolvePart1(exampleInput)
 
-	assert.Equal(t, 99999, result)
+	assert.Equal(t, 5, result)
 }
 
 func TestPart2(t *testing.T) {
